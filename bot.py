dbe447ac55a24d803bd3fb722e3a52c52f3f4a9f
Piparkuucinsh <audaja107@gmail.com>
2022-08-19 00:34:12 +0300

    disable join message

--- bot.py
+++ bot.py
@@ -322,7 +322,7 @@ async def post_user_newbest(score, score_rank, limit, scoretime, osu_user):
     await channel.send(embed=embed)
    
 
-@bot.event
+#@bot.event
 async def on_member_join(member):
     channel = bot.get_channel(266580155860779009)
     async with pool.acquire() as db:
